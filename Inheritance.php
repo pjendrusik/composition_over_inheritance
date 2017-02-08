ae048cc4d89b8ab25cc6b3588bc6690534bc05a8
Paweł Jendrusik <p.jendrusik@gmail.com>
2017-02-09 00:35:24 +0100

    alias classes in Inheritance.php as they are in Composition.php

--- Inheritance.php
+++ Inheritance.php
@@ -4,14 +4,14 @@ require_once "vendor/autoload.php";
 
 use \HellCatIT\Inheritance\Animal\Dog;
 use \HellCatIT\Inheritance\Animal\Cat;
-use \HellCatIT\Inheritance\Robot\Cleaning;
-use \HellCatIT\Inheritance\Robot\Murder;
-use \HellCatIT\Inheritance\Robot\MurderDog;
+use \HellCatIT\Inheritance\Robot\Cleaning as CleaningRobot;
+use \HellCatIT\Inheritance\Robot\Murder as MurderRobot;
+use \HellCatIT\Inheritance\Robot\MurderDog as MurderRobotDog;
 
 (new Dog())->poop()->bark();
 (new Cat())->poop()->meow();
 
-(new Cleaning())->drive()->clean();
-(new Murder())->drive()->kill();
+(new CleaningRobot())->drive()->clean();
+(new MurderRobot())->drive()->kill();
 
-(new MurderDog())->drive()->bark()->kill();
+(new MurderRobotDog())->drive()->bark()->kill();
